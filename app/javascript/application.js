88788c4dee5c9bd60e6f371a508f78dac51faf76
Rien <rien@croonenborghs.net>
2025-04-15 18:14:05 +1000

    remove importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 // Turborails does not update the browser's URL when you click around.
 document.addEventListener("turbo:click", (event) => {
   const url = event.detail.url;
